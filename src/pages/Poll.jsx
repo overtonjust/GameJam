82214b4d8edb6735648d800cfd13ebc1dd22f4d0
overtonjust <joverton@pursuit.org>
2024-05-06 13:52:34 -0400

    vote form draft

--- src/pages/Poll.jsx
+++ src/pages/Poll.jsx
@@ -1,26 +1,138 @@
 // Tools
 
 import './Poll.scss';
+import { useState, useRef } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Pie} from 'react-chartjs-2';
+import { Pie, getDatasetAtEvent } from 'react-chartjs-2';
 import { NavLink } from 'react-router-dom';
-import options from '../utils/pollData';
+import {pollData, updateData}  from '../utils/pollData';
+import Select from 'react-select';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 
 function Poll () {
-    
+    const [inputs,setInputs] = useState({});
+
+    const chartRef = useRef();
+
+
+    const votes = [
+        {name:'vote', value: 0 , label: "Chan's Peking"},
+        {name:'vote', value: 1 , label: 'Five Guys'},
+        {name:'vote', value: 2 , label: 'Golden Buddha'},
+        {name:'vote', value: 3 , label: 'Athena'},
+        {name:'vote', value: 4 , label: 'Gymkhana'},
+        {name:'vote', value: 5 , label: 'Hudson & Packard'},
+        {name:'vote', value: 6 , label: 'Mexicali Blue'},
+        {name:'vote', value: 7 , label: "Rossi's Deli"},
+        {name:'vote', value: 8 , label: "Sushi Village"},
+        {name:'vote', value: 9 , label: "Tamarind"}
+    ]
+
+    const handleInput = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name] : value }))
+    }
+
+    const handleSelect = (selectedOption) => {
+        const name = selectedOption.name;
+        const value = selectedOption.value;
+        setInputs(values => ({...values, [name] : value }))
+    }
+  
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        updateData(inputs.vote)
+    }
+
     return (
         <>
         <section className='vote'>
-            <form action="">
-
+            <form className='vote__form' onSubmit={handleSubmit} >
+                <h3 className='vote__title'>Takeaway Vote</h3>
+                <input 
+                    onChange={handleInput}
+                    type="text" 
+                    name='name'
+                    id='name' 
+                    className='vote__input' 
+                    placeholder='Please enter your name' 
+                    required 
+                />
+                <Select 
+                    onChange={handleSelect}
+                    options={votes}
+                    isClearable={true}
+                    required={true}
+                    placeholder={'Restaurant Choice'}
+                    styles={{
+                        control: (baseStyles, state) => ({
+                            ...baseStyles,
+                            borderRadius: '2em',
+                            width: 'calc(14 * 1vw)',
+                            height: 'calc(4 * 1vh)',
+                            backgroundColor: '#333333',
+                            boxShadow: '0 0 .1em #F4EDED',
+                            border: 'none',             
+                        }),
+                        placeholder: (base) => ({
+                            ...base,
+                            content: '"test"',
+                            color: '#F4EDED',
+                            fontSize: 'calc(.6* (1vh + 1vw))',
+                        }),
+                        menu: (base) => ({
+                            ...base,
+                            backgroundColor: '#333333',
+                            color: '#F4EDED',
+                            boxShadow: '0 0 .4em #8b8c89',
+                        }),
+                        option: (base) => ({
+                            ...base,
+                            color: '#F4EDED',
+                            backgroundColor: '#333333',
+                            cursor: 'pointer',
+                            ":active": {
+                                backgroundColor: '#333333',
+                            },
+                            ":hover": {
+                                color: '#9C23A9'
+                            }
+                        }),
+                        clearIndicator: (base) => ({
+                            ...base,
+                            color: '#F4EDED',
+                            cursor: 'pointer',
+                            ':hover': {
+                                color: '#9C23A9'
+                            }
+                        }),
+                        dropdownIndicator: (base) => ({
+                            ...base,
+                            color: '#F4EDED',
+                            cursor: 'pointer', 
+                            ':hover': {
+                                color: '#9C23A9'
+                            }
+                        }),
+                        singleValue: (base) => ({
+                            ...base,
+                            color: '#9C23A9',
+                        })
+                    }} 
+                 />
+                <input 
+                    type="submit" 
+                    className='vote__submit' 
+                />
                 <NavLink className='about__home' to='/'>
                     Go back home
                 </NavLink>
             </form>
-            <Pie data={options}/>
+            <Pie  data={pollData} />
         </section>
         </>
     )
--- src/utils/pollData.js
+++ src/utils/pollData.js
@@ -32,70 +32,21 @@ const borderColors= [
 
 
 const pollData = {
-    labels: ["Chan's Peking", "Five Guys","Greek", "Gymkhana","Hudson & Packard", "Mexicali Blue", "Rossi's Deli", "Sushi Village", "Tamarind"],
+    labels: ["Chan's Peking", "Five Guys", "Golden Buddha", "Athena", "Gymkhana","Hudson & Packard", "Mexicali Blue", "Rossi's Deli", "Sushi Village", "Tamarind"],
     datasets: [
         {
             label: "# of Votes",
-            data: [10,1,2,3,4,5,6,7,8],
+            names: ['test'],
+            data: [0,0,0,0,0,0,0,0,0,0],
             backgroundColor: backgroundColors,
             borderColor: borderColors,
             borderWidth: 1       
-        }         
+        }  ,      
     ]
 }
 
-export default pollData;
+function updateData(index){
+   return pollData.datasets[0].data[index]++
+}
 
-/*"image": {
-    "src": "../src/assets/poll-images/chinese.png",
-    "alt": "Bowl of general tsos chicken surrounded by broccoli"
-}
-},
-{
-"image": {
-    "src": "../src/assets/poll-images/five-guys.png",
-    "alt": "Five guys cheeseburger, fries and milkshake"
-}
-},
-{
-"image": {
-    "src": "../src/assets/poll-images/Greek.png",
-    "alt": "Close up of a gyro sandwich"
-}
-},
-{
-"image": {
-    "src": "../src/assets/poll-images/Gymkhana.png",
-    "alt": "Man looking at blown up spread of indian food."
-}
-},
-{
-"image": {
-    "src": "../src/assets/poll-images/pizza.png",
-    "alt": "Zoom in angle shot of pesto and cheese pizza."
-}
-},
-{
-"image": {
-    "src": "../src/assets/poll-images/mexican.png",
-    "alt": "A burrito cut in half with one half leaning on the other."
-}
-},
-{
-"image": {
-    "src": "../src/assets/poll-images/rossis.png",
-    "alt": "Chick parmesan sandwich on muffaletta bread, cut into four slices with one slice removed."
-}
-},
-{
-"image": {
-    "src": "../src/assets/poll-images/sushi.png",
-    "alt": "Angled rectangular plate filled with sushi topped with avocado and tobiko."
-}
-},
-{
-"image": {
-    "src": "../src/assets/poll-images/tamarind.png",
-    "alt": "Bowl of orange curry with the Tamarind logo on the bowl handle."
-}
-}*/
+export {pollData ,updateData};
